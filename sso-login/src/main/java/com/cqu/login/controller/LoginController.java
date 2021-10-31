a0ffde027c435ef882e025decd4c8f1155e08f19
LoftyComet <1277173875@qq.com>
2021-10-31 15:41:46 +0800

    登录失败返回登录界面，使用cookie保存用户登录信息

--- sso-login/src/main/java/com/cqu/login/controller/LoginController.java
+++ sso-login/src/main/java/com/cqu/login/controller/LoginController.java
@@ -1,19 +1,26 @@
 package com.cqu.login.controller;
 
+import java.net.http.HttpResponse;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.cqu.login.pojo.User;
 import com.cqu.login.utils.LoginCacheUtil;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/login")
@@ -27,7 +34,7 @@ public class LoginController {
     } // 模拟用户数据库
 
     @PostMapping
-    public String doLogin(User user,HttpSession session){
+    public String doLogin(User user, HttpSession session, HttpServletResponse response){
         String target = (String) session.getAttribute("target");
         Optional<User> first = dbUsers.stream().filter(dbUser ->dbUser.getUsername()
             .equals(user.getUsername())&&dbUser.getPassword().equals(user.getPassword()))
@@ -35,15 +42,32 @@ public class LoginController {
         if (first.isPresent()){
             // 保存用户登陆信息
             String token = UUID.randomUUID().toString();
-            LoginCacheUtil.loginUser.put(token,first.get());
-
+            // 创建需要的cookies并返回
+            Cookie cookie = new Cookie("TOKEN", token);
+            cookie.setDomain("localhost"); //设定域名
+            response.addCookie(cookie);
+            LoginCacheUtil.loginUser.put(token, first.get());
         }
         else{
             // 登陆失败
             session.setAttribute("msg", "用户名或密码错误");
             return "login";
         }
+        
         // 重定向到target地址
         return "redirect:" + target;
+    }
+
+    @GetMapping("info")
+    @ResponseBody
+    public ResponseEntity<User> getUserInfo(String token) {
+        if (!StringUtils.isEmpty(token)){
+            User user = LoginCacheUtil.loginUser.get(token);
+            return ResponseEntity.ok(user);
+            
+        }
+        else {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- sso-login/src/main/java/com/cqu/login/controller/ViewController.java
+++ sso-login/src/main/java/com/cqu/login/controller/ViewController.java
@@ -2,9 +2,16 @@ package com.cqu.login.controller;
 
 import org.thymeleaf.util.StringUtils;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
+import com.cqu.login.pojo.User;
+import com.cqu.login.utils.LoginCacheUtil;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +23,19 @@ public class ViewController {
     // 跳转到登陆界面
     @GetMapping("/login")
     public String toLogin(@RequestParam(required = false,defaultValue = "") String target 
-                                                ,HttpSession session) {
+                                ,HttpSession session,@CookieValue(required = false,value = "TOKEN") Cookie cookie) {
         if (StringUtils.isEmpty(target)) {
             target = "http://localhost:9010";
         }
+        // 如果用户已经登录再次访问登录页面，重定向       
+        if (cookie != null) {
+            String value = cookie.getValue();
+            User user = LoginCacheUtil.loginUser.get(value);
+            if (user != null) {
+                return "redirect:" + target;
+            }
+        }
+        
         // 要做target地址是否合法的检验
         // 重定向地址
         session.setAttribute("target", target); //将地址存起来
